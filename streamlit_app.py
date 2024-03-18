802b04fd01dcd705c6d5b3c6b23e7320a902c82f
shilpaleo <shilpa.sindhe87@gmail.com>
2024-03-19 00:20:00 +0800

    test completion sync

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,23 @@ import pandas as pd
 dim_user = pd.read_pickle('./data/dim_user.pkl')
 dim_product = pd.read_pickle('./data/dim_product.pkl')
 fact_order = pd.read_pickle('./data/fact_order.pkl')
+sql_df1 = pd.read_pickle('./data/sql_df1.pkl')
+sql_df2 = pd.read_pickle('./data/sql_df2.pkl')
 
 # Streamlit app title
-st.title('Sales insights dashboard')
+st.title('Sales insights')
 
-# Streamlit app charts
+# Answers to the questions
+st.header('Answers to the questions')
+st.subheader('1. Which user spent the most money on products on all Fridays?')
+st.dataframe(sql_df1)
+
+st.subheader('2. What are the best 3 products in each location of a user based on quantity?')
+st.dataframe(sql_df2)
+
+# Charts
+st.header('Visualizations')
+
 # Chart 1: Total sales over time
 date_grouped_df = fact_order.groupby("order_created")["order_total"].sum().reset_index()
 
@@ -19,40 +31,31 @@ fig1 = px.line(
     date_grouped_df.iloc[1:-1],
     x="order_created",
     y="order_total",
-    title="Total Sales Over Time",
+    title="SalesTrend Over Time",
 )
 st.plotly_chart(fig1, use_container_width=True)
-
-# Chart 2: Total basket size over time
-date_grouped_basket_df = fact_order.groupby("order_created")["basket_size"].sum().reset_index()
-
-fig2 = px.line(
-    date_grouped_basket_df.iloc[1:-1],
-    x="order_created",
-    y="basket_size",
-    title="Basket size Over Time",
-)
-st.plotly_chart(fig2, use_container_width=True)
 
 # Adding columns for two charts
 col1, col2 = st.columns(2)
 
-# Chart 3: Total sales by User
+# Chart 2: Top Spenders
 with col1:
     user_grouped_df = (
-        fact_order.groupby("user_id")["order_total"].sum().reset_index()
+        fact_order.merge(dim_user, on="user_id")
+        .groupby("user_name")["order_total"].sum().reset_index()
         .sort_values("order_total", ascending=False)
         .head()
     )
 
-    fig3 = px.bar(
+    fig2 = px.bar(
         user_grouped_df,
-        y="user_id",
+        y="user_name",
         x="order_total",
-        title="Total Sales By User",
+        title="Top Sales By User",
         orientation='h',
+        width=1000,
     ).update_yaxes(type='category', categoryorder='max ascending')
-    st.plotly_chart(fig3, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
 
 with col2:
     user_city_sales = (
@@ -60,10 +63,10 @@ with col2:
     .groupby("user_city")["order_total"].sum().reset_index()
     )
 
-    fig4 = px.pie(
+    fig3 = px.pie(
         user_city_sales,
         values="order_total",
         names="user_city",
         title="Total Sales By City",
     )
-    st.plotly_chart(fig4, use_container_width=True)    
+    st.plotly_chart(fig3, use_container_width=True)    
